bb8481e3ec88049ab1c18c14f4a9ae73f8ea3ef1
ConsolLog <jainaman06060@gmail.com>
2021-10-28 19:46:10 +0530

    complete the app

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_constructors_in_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
@@ -9,9 +9,10 @@ class Question extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: const EdgeInsets.all(10),
       child: Text(
         questionText,
-        style: TextStyle(fontSize: 28),
+        style: const TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
     );
---
+++ lib/result.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+  
+  String get resultPharse {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = "You are awesome and  innocent!";
+    } else if (resultScore <= 12) {
+      resultText = "Pretty likeable";
+    } else if (resultScore <= 16) {
+      resultText = "You are ... strange?!";
+    } else {
+      resultText = "You are so bad!";
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPharse,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restsrt Quiz!'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          )
+        ],
+      ),
+    );
+  }
+}
